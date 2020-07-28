f3a7fb3ba6b47694c935833ea20db7e7de70f3eb
Hariom Sharma <hariom@appointy.com>
2020-07-28 19:36:41 +0530

    fix: convert to html

--- src/lib/utils.tsx
+++ src/lib/utils.tsx
@@ -1,6 +1,11 @@
+// @ts-ignore
+import { convertToHTML as _ } from 'draft-convert'
+import { convertFromRaw, RawDraftContentState } from 'draft-js'
 import { WysiwygState } from './types'
 
 export const createWysiwygState = (): WysiwygState => ({
     blocks: [],
     entityMap: {}
 })
+
+export const convertToHTML = (input: RawDraftContentState): string => _(convertFromRaw(input))
